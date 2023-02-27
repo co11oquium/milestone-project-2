ccff37b4fc78b8ec73df828d80b33c567b63a665
co11oquium <122843308+co11oquium@users.noreply.github.com>
2023-02-27 12:46:44 +0000

    Migrate to Swift 4.2

--- test/fetch_testdata.sh
+++ test/fetch_testdata.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
 
+try_download() {
+  local testdata=$1
+  local source=$2
+  if [ -f "$testdata" ]; then
+      echo "$testdata already exists"
+  else
+      echo "$testdata does not exist, downloading..."
+      curl "$source" -o "$testdata"
+  fi
+}
+
+try_download d
